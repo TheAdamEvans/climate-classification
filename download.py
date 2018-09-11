be85e15426fac0dd9aefeccaf63b2fdddde99072
Ubuntu <ubuntu@ip-172-31-32-69.ap-southeast-2.compute.internal>
2018-09-11 01:33:31 +0000

    bit more pathing

--- download.py
+++ download.py
@@ -1,27 +1,31 @@
 from subprocess import call
+from pathlib import Path
 import sys
 
-def download_year_data(year):
+def download_year_data(year, path = Path('./data')):
     """
     one year of hourly data from NOAA
     """
-    url = [f'https://www.ncei.noaa.gov/data/global-hourly/archive/{year}.tar.gz']
+    url = f'https://www.ncei.noaa.gov/data/global-hourly/archive/{year}.tar.gz'
     tarball = f'{year}.tar.gz'
 
+    (path/'downloads/').mkdir(parents=True, exist_ok=True)
+
     # download from NOAA
-    call(["wget"]+url+['-O']+[tarball]+['-c'])
+    call(["wget"]+[url]+['-O']+[tarball]+['-c'])
+    call(['mv']+[tarball]+[(path/'downloads/.')])
 
     # uncompress
-    call(['mkdir',f'./data/{year}'])
-    call(['tar','-xzf',tarball,'-C',f'./data/{year}'])
+    # call(['mkdir',f'./data/{year}'])
+    (path/f'{year}').mkdir(parents=True, exist_ok=True)
+    call(['tar','-xzf',(path/'downloads'/tarball),'-C',(path/f'{year}')])
 
     # clean up
-    call(['rm',tarball])
+    # call(['rm',tarball])
 
 
 if len(sys.argv) < 2:
-    for year in range(1973, 2000):
+    for year in range(1973, 1980):
         download_year_data(year)
 else:
-    # TODO make data folder if it doesn't exist
     download_year_data(sys.argv[1])
